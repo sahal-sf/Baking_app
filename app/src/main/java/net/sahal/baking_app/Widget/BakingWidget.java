a5a879e21b53ce8da2f4e185be4c4d18a5207ab9
MeeDooBak <medo2011r@gmail.com>
2019-11-10 19:03:40 +0300

    Widget view added

---
+++ app/src/main/java/net/sahal/baking_app/Widget/BakingWidget.java
@@ -0,0 +1,62 @@
+package net.sahal.baking_app.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import net.sahal.baking_app.MainActivity;
+import net.sahal.baking_app.R;
+import net.sahal.baking_app.models.BakingList;
+import net.sahal.baking_app.models.Ingredients;
+
+import java.util.ArrayList;
+
+public class BakingWidget extends AppWidgetProvider {
+
+    private static final ArrayList<BakingList> List = MainActivity.List;
+    private static int position;
+    private static String result = "";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        position = BakingWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
+
+        if (position > -1) {
+            ArrayList<Ingredients> ingredient = List.get(position).getIngredients();
+
+            for (int i = 0; i < ingredient.size(); i++) {
+                result += " " + (i + 1) + " - " + ingredient.get(i).getQuantity() + " " +
+                        ingredient.get(i).getMeasure() + " ( " + ingredient.get(i).getIngredient() + " )";
+                if (i < ingredient.size() - 1) {
+                    result += "\n";
+                }
+            }
+            views.setTextViewText(R.id.appwidget_text, result);
+        }
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            BakingWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+    }
+}
+
---
+++ app/src/main/java/net/sahal/baking_app/Widget/BakingWidgetConfigureActivity.java
@@ -0,0 +1,96 @@
+package net.sahal.baking_app.Widget;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import net.sahal.baking_app.MainActivity;
+import net.sahal.baking_app.R;
+import net.sahal.baking_app.models.BakingList;
+
+import java.util.ArrayList;
+
+public class BakingWidgetConfigureActivity extends Activity {
+
+    private static final String PREFS_NAME = "net.sahal.baking_app.Widget.BakingWidget";
+    private static final String PREF_PREFIX_KEY = "appwidget_";
+    private final ArrayList<BakingList> List = MainActivity.List;
+    private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private RadioGroup radioGroup;
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            final Context context = BakingWidgetConfigureActivity.this;
+
+            System.out.println(radioGroup.getCheckedRadioButtonId() + " --jdasdfjjnjasdfnjnj");
+
+            saveTitlePref(context, mAppWidgetId, radioGroup.getCheckedRadioButtonId());
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            BakingWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    };
+
+    public BakingWidgetConfigureActivity() {
+        super();
+    }
+
+    static void saveTitlePref(Context context, int appWidgetId, int position) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putInt(PREF_PREFIX_KEY + appWidgetId, position);
+        prefs.apply();
+    }
+
+    static int loadTitlePref(Context context, int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        int position = prefs.getInt(PREF_PREFIX_KEY + appWidgetId, -1);
+        return position;
+    }
+
+    static void deleteTitlePref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId);
+        prefs.apply();
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setResult(RESULT_CANCELED);
+
+        setContentView(R.layout.baking_widget_configure);
+
+        radioGroup = findViewById(R.id.radio_group);
+        for (int i = 0; i < List.size(); i++) {
+            RadioButton radioBtn = new RadioButton(this);
+            radioBtn.setId(i);
+            radioBtn.setText(List.get(i).getName());
+            radioGroup.addView(radioBtn);
+        }
+        radioGroup.check(0);
+
+        findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+            return;
+        }
+    }
+}
+
